c6a8c1380f203c9bf5099676af4042e69cd1126c
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-06-09 00:55:03 -0600

    PWA config v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+<link
+    rel="manifest"
+    href="/manifest.json"
+/>
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
