40ba6c7121c6f6dbbc2daefde2686a6c41c86f76
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-06-09 01:58:30 -0600

    Next config change
    next.config.js file change

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
+
+///** @type {import('next').NextConfig} */
+//const nextConfig = {}
+
+//module.exports = nextConfig
+
+//const withPWA = require('next-pwa')({
+//    dest: 'public'
+//  })
+  
+//  module.exports = withPWA({
+    // next.js config
+//  })
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
-
-const withPWA = require('next-pwa')({
-    dest: 'public'
-  })
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  });
   
   module.exports = withPWA({
-    // next.js config
-  })
+    reactStrictMode: true,
+  });
+
+
+  
