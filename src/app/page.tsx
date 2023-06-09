4e85ec38501d8ec51192f2722740d9cff13f7dff
Víctor Ponce <107383836+victor-ponce@users.noreply.github.com>
2023-06-08 22:35:42 -0600

    Button added
    Hello World button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import React, { FC } from 'react';
+import MyButton from '../components/button';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+          <MyButton />
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
---
+++ src/components/button.tsx
@@ -0,0 +1,16 @@
+import React, { MouseEvent, FC } from 'react';
+import Button from '@mui/material/Button';
+
+const MyButton: FC = () => {
+    const handleClick = (event: MouseEvent<HTMLButtonElement>): void => {
+        alert('Hola Mundo');
+    }
+
+    return (
+        <Button variant="contained" color="primary" onClick={handleClick}>
+            Push me
+        </Button>
+    );
+}
+
+export default MyButton;
